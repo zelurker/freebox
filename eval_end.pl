2a6a765f6ac527e4afe54e67d200273a5995989d
Emmanuel Anne <emmanuel.anne@gmail.com>
2011-08-19 23:48:41 +0200

    fichier externe

--- eval_end.pl
+++ eval_end.pl
@@ -1,10 +1,14 @@
 #!/usr/bin/perl
 
-if (open(F,"mplayer -frames 1 -vo null -ao null -identify stream.dump|")) {
+use strict;
+
+my $name=`head -n 7 current|tail -n 1`;
+chomp $name;
+if (open(F,"mplayer -frames 1 -vo null -ao null -identify '$name'|")) {
 	while (<F>) {
 		if (/ID_LENGTH=(\d+)/) {
 			my $len = $1;
-			$len -= 0.5;
+			$len -= 0.9;
 			$len = 0 if ($len < 0);
 			print "$len\n";
 			last;
