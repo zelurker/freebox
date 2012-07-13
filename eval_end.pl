81f59e15a6398286165bab2a65048e870ed5a06e
Emmanuel Anne <emmanuel.anne@gmail.com>
2012-07-14 01:13:33 +0200

    Pour certaines vidéos il faut utiliser mplayer2

--- eval_end.pl
+++ eval_end.pl
@@ -7,17 +7,31 @@ use strict;
 
 my $name=`head -n 7 current|tail -n 1`;
 chomp $name;
+my $len;
 if (open(F,"mplayer -frames 1 -vo null -ao null -identify '$name'|")) {
 	while (<F>) {
 		if (/ID_LENGTH=(\d+)/) {
-			my $len = $1;
+			$len = $1;
 			$len -= 0.9;
 			$len = 0 if ($len < 0);
-			print "$len\n";
 			last;
 		}
 	}
 	close(F);
+	if ($len == 0) {
+		if (open(F,"mplayer2 -frames 1 -vo null -ao null -identify '$name'|")) {
+			while (<F>) {
+				if (/ID_LENGTH=(\d+)/) {
+					$len = $1;
+					$len -= 0.9;
+					$len = 0 if ($len < 0);
+					last;
+				}
+			}
+			close(F);
+		}
+	}
+	print "$len\n";
 } else {
 	print "0\n";
 }
