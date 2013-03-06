aaa545894a073ec43b2d1e8bdf2aa9ee31108157
Emmanuel Anne <emmanuel.anne@gmail.com>
2013-03-06 16:55:25 +0100

    Plus de logos pour les radios
    et un script pour les récupérer en auto sur wikipedia. Ca récupère pas tout
    évidemment, mais ça en fait déjà pas mal... et c'est joli !

--- radios.pl
+++ radios.pl
@@ -22,7 +22,23 @@ use strict;
 use warnings;
 
 my %icons = (
-	"MFM" => "http://upload.wikimedia.org/wikipedia/fr/b/bb/Logo-mfm.png",
+"Activ radio" => "http://upload.wikimedia.org/wikipedia/fr/8/85/Activ_radio_2012_logo.png",
+"BFM" => "http://upload.wikimedia.org/wikipedia/fr/0/0d/BFM_Business_logo_2010.png",
+"Bide et Musique" => "http://upload.wikimedia.org/wikipedia/commons/f/f7/BM_icone.png",
+"Demoiselle FM" => "http://upload.wikimedia.org/wikipedia/fr/e/e2/Logo_Demoiselle_FM_%282012%29.jpg",
+"FIP" => "http://upload.wikimedia.org/wikipedia/fr/3/35/Logo_Fip.jpg",
+"France bleu Gascogne" => "http://upload.wikimedia.org/wikipedia/fr/4/43/France_Bleu_Gascogne.jpg",
+"France Culture" => "http://upload.wikimedia.org/wikipedia/fr/5/55/Logo_France_Culture.png",
+"France Musique" => "http://upload.wikimedia.org/wikipedia/fr/6/64/France_Musique_logo_2008.png",
+"Fun Radio" => "http://upload.wikimedia.org/wikipedia/fr/e/eb/Fun_Radio.png",
+"Graffiti Urban Radio" => "http://upload.wikimedia.org/wikipedia/fr/8/86/Graffiti_Radio.jpg",
+"Hit West" => "http://upload.wikimedia.org/wikipedia/fr/b/bd/HitWestLogo.jpg",
+"Le Mouv'" => "http://upload.wikimedia.org/wikipedia/fr/5/57/Logo_mouv_2005.png",
+"MFM" => "http://upload.wikimedia.org/wikipedia/fr/b/bb/Logo-mfm.png",
+"Nostalgie" => "http://upload.wikimedia.org/wikipedia/fr/9/9d/Logo_Nostalgie.png",
+"Oxyradio" => "http://upload.wikimedia.org/wikipedia/commons/e/e1/Oxyradio-logo.png",
+"Prun" => "http://upload.wikimedia.org/wikipedia/fr/0/02/Logo-prun-3wprunnet-%2B-92fm.jpg",
+"Top Music" => "http://upload.wikimedia.org/wikipedia/fr/a/a2/Top_Music_logo.png",
 );
 
 sub get_radio_pic {
