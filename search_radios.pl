0663f2ae2a3dcbffc50c17b6c16512b56e874830
Emmanuel Anne <emmanuel.anne@gmail.com>
2017-04-20 18:32:59 +0200

    mÃ j des stations
    search_radios essaye de chercher les flux tout seul, Ã§a marche des
    fois... !
    Ca fait quand mÃªme gagner un peu de temps.

--- search_radios.pl
+++ search_radios.pl
@@ -5,30 +5,103 @@ use warnings;
 use WWW::Mechanize;
 require "radios.pl";
 use Data::Dumper;
+use v5.10;
 use strict;
 
 $| = 1;
 my %icons = get_icons();
+our %flux;
+
 sub disp_stations {
 	print "\n";
+	say "flux:";
+	foreach (sort { $a cmp $b } keys %flux) {
+		print "  \"$_\" => \"$flux{$_}\",\n"
+	}
+	print "\n";
+
+	say "icons:";
 	foreach (sort { $a cmp $b } keys %icons) {
 		print "  \"$_\" => \"$icons{$_}\",\n"
 	}
 }
-my $site_addr = "guidetv-iphone.telerama.fr";
-my $mech = WWW::Mechanize->new();
+
+sub init_mech {
+	my $mech = WWW::Mechanize->new();
 #$mech->agent_alias("Linux Mozilla");
-$mech->agent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_5) AppleWebKit/537.71 (KHTML, like Gecko) Version/6.1 Safari/537.71");
-$mech->timeout(10);
-$mech->default_header('Accept-Encoding' => scalar HTTP::Message::decodable());
+	$mech->agent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_5) AppleWebKit/537.71 (KHTML, like Gecko) Version/6.1 Safari/537.71");
+	$mech->timeout(10);
+	$mech->default_header('Accept-Encoding' => scalar HTTP::Message::decodable());
+	$mech->max_size(130000); # 65000 trop court pour google !
+	$mech;
+}
+
+sub search_flux {
+	my ($url,$visited,$mech) = @_;
+	$visited = [] if (!defined($visited));
+	push @$visited,$url;
+	$mech = init_mech() if (!$mech);
+	say "search_flux: entering with url ",$url;
+	my $page = $mech->get($url);
+	my $page = $mech->content;
+	if ($page =~ /(file|mp3|m4a): ["'](.+?)["']/ || $page =~ /(data-src)="(.+?)"/ || $page =~ /(url)&quot;:&quot;(http.+?)&quot;/) {
+		say "search_flux : found $1 info $2";
+		return $2;
+	}
+	foreach ($mech->links) {
+		my $link = $_->url;
+		if ($_->text && $_->text =~ /coute/ && !grep { $_ eq $link } @$visited) {
+			say "search_flux: selected ",$_->text," url ",$_->url;
+			return search_flux($_->url,$visited,$mech);
+		}
+	}
+	$mech->save_content("page.html");
+	die "search_flux page saved";
+}
+
+my $mech = init_mech();
 
 open(my $i,"<flux/stations") || die "can't read stations\n";
+my $last;
 while (<$i>) {
 	chomp;
 	my $url = <$i>;
+	my $prog;
+	($url,$prog) = split(/ /,$url);
 	my $station = $_;
-	next if ($station ne "MFM Lady");
+	next if ($last && $station ne $last);
+	$last = undef;
 	my $adr = $icons{$station};
+	print "$_: vérif flux... ";
+	eval {
+		$mech->head($url);
+	};
+	if ($@ && $@ =~ /bad request/i) {
+		print "can't head, trying get... ";
+		$mech->get($url);
+	}
+	if (!$@) {
+		say "ok";
+	} else {
+		say "pas ok : $!: $@";
+		$mech = search_station("radio $station");
+		foreach ($mech->links) {
+			next if ($_->url =~ /(streama|ecouterradio|ecouter-en-direct.com|ecouter-la-radio.fr)/);
+			if ($_->text && $_->text =~ /(coutez|player|direct)/i) {
+				my $flux = search_flux($_->url);
+				$flux{$station} = $flux.($prog ? " $prog" : "");
+				last;
+			}
+		}
+		if (!$flux{$station}) {
+			say "pas trouvé de flux pour $station. Liens :\n";
+			foreach ($mech->links) {
+				say $_->text if ($_->text);
+			}
+			$mech->save_content("page.html");
+			exit(0);
+		}
+	}
 	if ($adr) {
 		print "j'ai déjà $_, check... ";
 		eval {
@@ -36,42 +109,14 @@ while (<$i>) {
 		};
 		if (!$@) {
 			print "ok\n";
+			delete $icons{$station};
 			next;
 		} else {
 			print "$!: $@\n";
 			delete $icons{$station};
 		}
 	}
-	print "on étudie $station... ";
-	my $q = "logo $station".(/radio/i ? "" : " radio");
-	my $r;
-	do {
-		eval {
-			$mech->get("https://www.google.fr/");
-			$r = $mech->submit_form(
-				form_number => 1,
-				fields      => {
-					q => $q,
-				}
-			);
-		};
-		if ($@) {
-			print "Erreur $!: $@\n";
-			my @forms = $mech->forms;
-			for (my $n=0; $n<=$#forms; $n++) {
-				print "$n: ",Dumper($forms[$n]),"\n";
-			}
-			print "sleeping 3s... ";
-			sleep 3;
-			print "\n";
-			undef $mech;
-			$mech = WWW::Mechanize->new();
-#$mech->agent_alias("Linux Mozilla");
-			$mech->agent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_5) AppleWebKit/537.71 (KHTML, like Gecko) Version/6.1 Safari/537.71");
-			$mech->timeout(10);
-			$mech->default_header('Accept-Encoding' => scalar HTTP::Message::decodable());
-		}
-	} while ($@);
+	$mech = search_station("logo $station".(/radio/i ? "" : " radio"));
 	foreach ($mech->links) {
 		my ($sub) = $_->url =~ /q=(.+?)\&/;
 		($sub) = $_->url =~ /url=(.+?)\&/ if (!$sub);
@@ -101,4 +146,38 @@ while (<$i>) {
 	}
 }
 
-disp_stations();
+sub search_station {
+	my ($texte) = @_;
+	my $q = $texte;
+	my $r;
+	my $mech = init_mech();
+	do {
+		eval {
+			$mech->get("https://www.google.fr/");
+			$r = $mech->submit_form(
+				form_number => 1,
+				fields      => {
+					q => $q,
+				}
+			);
+		};
+		if ($@) {
+			print "Erreur $!: $@\n";
+			my @forms = $mech->forms;
+			for (my $n=0; $n<=$#forms; $n++) {
+				print "$n: ",Dumper($forms[$n]),"\n";
+			}
+			print "sleeping 3s... ";
+			sleep 3;
+			print "\n";
+			undef $mech;
+			$mech = init_mech();
+		}
+	} while ($@);
+	$mech;
+}
+
+END {
+	disp_stations();
+}
+
