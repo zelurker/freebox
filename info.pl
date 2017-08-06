475d5cd90de61827deb94dc143e4d7e616c97087
Emmanuel Anne <emmanuel.anne@gmail.com>
2017-08-07 00:39:15 +0200

    is_utf8 est foireux, comme d'hab, on tente notre dÃ©tection

--- info.pl
+++ info.pl
@@ -454,8 +454,20 @@ sub encoding {
 		}
 		$$ref =~ s/\x{2019}/'/g;
 		$$ref =~ s/\x{0153}/oe/g;
+		# Ok, on va chercher l'encodage de la chaine puisqu'on ne peut
+		# faire confiance à is_utf8. L'idée c'est de se fier aux codes
+		# ascii, on cherche le maxi dans la chaine.
+		# Si il est < 128 alors c'est de l'ascii standard, pas besoin
+		# d'encodage
+		# Si c'est < 256 alors c'est du latin1
+		# Si c'est > 256 alors c'est de l'utf8 (j'espère !)
+		my $max_ord = 0;
+		for (my $n=0; $n<length($$ref); $n++) {
+			$max_ord = ord(substr($$ref,$n,1)) if (ord(substr($$ref,$n,1)) > $max_ord);
+		}
+		next if ($max_ord < 128);
 		if (!$latin) {
-			if ($$ref =~ /[\xc3\xc5]/ || utf8::is_utf8($$ref)) {
+			if ($$ref =~ /[\xc3\xc5]/ || $max_ord > 255) {
 				# print "to_utf: reçu un truc en utf: $$ref\n";
 				next;
 			}
