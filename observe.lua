c6a3a09fc1dadc98548b55c8a2df6cc32af3809e
Emmanuel Anne <emmanuel.anne@gmail.com>
2018-06-24 11:45:03 +0200

    observe.lua : affichage auto du bandeau d'info

--- observe.lua
+++ observe.lua
@@ -5,6 +5,10 @@ function on_width_change(name,value)
 	file = io.open("video_size", "w")
 	file:write(string.format("%d\n%d\n",value,height))
 	file:close()
+	os.remove("list_coords")
+	os.remove("info_coords")
+	os.remove("numero_coords")
+	os.execute("./info")
 end
 
 mp.observe_property("osd-width","number",on_width_change)
