cd8b7cdada388e29c007475527d15acf25b0c45b
Emmanuel Anne <emmanuel.anne@gmail.com>
2018-06-28 20:59:37 +0200

    observe.lua: ajout d'une pause pour l'info et metadata
    note : la partie perl n'est pas encore faite pour la metadata

--- observe.lua
+++ observe.lua
@@ -1,4 +1,6 @@
 local mp = require 'mp'
+socket = require "socket"
+socket.unix = require"socket.unix"
 
 function on_width_change(name,value)
 	local file = io.open("video_size","r")
@@ -11,16 +13,47 @@ function on_width_change(name,value)
 	if (value > 0 and tonumber(height) > 0) then
 		file = io.open("video_size", "w")
 		file:write(string.format("%d\n%d\n",value,height))
+		par = mp.get_property("osd-par")
+		print("on_width_change : " .. value .. " " .. height .. " par " .. par)
 		file:close()
 		if exists then
 			return
 		end
 		os.remove("info_coords")
-		os.execute("./info short")
+		-- on insère l'appel dans une pause de 1s parce que la video se
+		-- configure 2 fois très rapidement quand lancé en plein écran !
+		mp.add_timeout(1,function ()
+			os.execute("./info short")
+		end)
+	end
+end
+
+function metadata(name,value)
+	for i,v in pairs(value) do
+		print(i,v)
+	end
+end
+
+function on_abitrate(name,value)
+	local vcodec = mp.get_property("video-codec")
+	if not vcodec then
+		local acodec = mp.get_property("audio-codec-name")
+		if not acodec then
+			return
+		end
+		local c = assert(socket.unix())
+		if (c:connect("sock_info")) then
+			c:send("codec " .. acodec .. " " .. (value/1000) .. "k")
+			c:close()
+		else
+			print("acodec " .. acodec .. " bitrate " .. (value/1000) .. "k")
+		end
 	end
 end
 
 mp.observe_property("osd-width","number",on_width_change)
+mp.observe_property("audio-bitrate","number",on_abitrate)
+mp.observe_property("metadata","native",metadata)
 
 mp.add_hook("on_unload", 10, function ()
 	pos = mp.get_property_number("percent-pos")
@@ -28,20 +61,19 @@ mp.add_hook("on_unload", 10, function ()
 		-- si on lit une url, on aura pas de pos ici
 		return
 	end
-	socket = require"socket"
-	socket.unix = require"socket.unix"
-	c = assert(socket.unix())
-	assert(c:connect("sock_list"))
-	name = mp.get_property("path")
-	if (pos < 90 or pos > 100) then
-		-- gestion basique des bookmarks :
-		-- on ne semble pas pouvoir modifier directement le fichier
-		-- en lua, donc on transmet la commande à list.pl
-		pos = mp.get_property("time-pos")
-		c:send("bookmark " .. name .. " " .. pos .. "\n")
-	else
-		c:send("bookmark " .. name .. " del\n")
+	local c = assert(socket.unix())
+	if (c:connect("sock_list")) then
+		name = mp.get_property("path")
+		if (pos < 90 or pos > 100) then
+			-- gestion basique des bookmarks :
+			-- on ne semble pas pouvoir modifier directement le fichier
+			-- en lua, donc on transmet la commande à list.pl
+			pos = mp.get_property("time-pos")
+			c:send("bookmark " .. name .. " " .. pos .. "\n")
+		else
+			c:send("bookmark " .. name .. " del\n")
+		end
+		c:close()
 	end
-	c:close()
 end)
 
