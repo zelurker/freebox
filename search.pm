46553621d7005bb5273dfa7247bf8b2d56d95bf0
Emmanuel Anne <emmanuel.anne@gmail.com>
2019-05-13 00:15:33 +0200

    search: sÃ©pare l'init de $mech
    pour pouvoir Ãªtre appelÃ©e juste pour l'init (series fait Ã§a)

--- search.pm
+++ search.pm
@@ -2,14 +2,19 @@ package search;
 
 use WWW::Mechanize;
 
-sub search {
-	my $q = shift;
-
+sub init {
 	my $mech = WWW::Mechanize->new();
 	$mech->agent_alias("Linux Mozilla");
 	# $mech->agent("Mozilla/5.0 (X11; Linux x86_64; rv:57.0) Gecko/20100101 Firefox/57.0");
 	$mech->timeout(12);
 	$mech->default_header('Accept-Encoding' => scalar HTTP::Message::decodable());
+	$mech;
+}
+
+sub search {
+	my $q = shift;
+
+	my $mech = init();
 	eval {
 		$mech->get("https://www.duckduckgo.com/");
 		$mech->submit_form(
