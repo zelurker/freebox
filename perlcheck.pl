d57c98e82fcc151095004b50e1788a916e486c98
Emmanuel Anne <emmanuel.anne@gmail.com>
2019-09-25 01:58:07 +0200

    script d'install auto des modules cpan
    et on laisse le choix entre zenity ou gtkdialog vu que gtkdialog n'est
    pas dispo en ce moment dans arch !

---
+++ perlcheck.pl
@@ -0,0 +1,27 @@
+#!/usr/bin/perl
+
+use strict;
+use v5.10;
+
+my $r;
+do {
+	$r = system("perl -c $ARGV[0] >& log");
+	my $handled = 0;
+	if ($r >> 8) {
+		open(F,"<log") || die "can't open log";
+		while(<F>) {
+			if (/Can't locate (.+?) in/) {
+				$handled = 1;
+				say "must install $1";
+				my $r2 = system("cpan -T $1");
+				die "cpan call failed" if ($r2);
+			}
+		}
+		close(F);
+		if (!$handled) {
+			system("cat log");
+			die "can't handle this error";
+		}
+	}
+} while ($r);
+
