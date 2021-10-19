3c1afc62495fa3b8d8788680e5862bab766bedd9
Emmanuel Anne <emmanuel.anne@gmail.com>
2021-10-19 14:58:23 +0200

    images: utilise http au lieu de WWW::Mechanize
    Ã  cause d'encore un pb utf, en cas de fichier utf le nom est encodÃ© 2
    fois ! Il doit y avoir un moyen d'Ã©viter Ã§a, mais la mÃ©thode simple
    c'est de passer Ã  http, en + les erreurs ne sont + fatales au moins.
    Et au passage "sanitize" les noms de fichiers.

--- images.pm
+++ images.pm
@@ -3,23 +3,23 @@
 package images;
 
 use Coro::LWP;
-use WWW::Mechanize;
 use Cpanel::JSON::XS qw(decode_json);
 use common::sense;
+use http;
 
 our $debug = 0;
 
 sub search {
 	my ($self,$q) = @_;
-	my $mech;
 	my @tab = ();
-	$mech = WWW::Mechanize->new();
-	$mech->agent("Mozilla/5.0 (X11; Linux x86_64; rv:45.0) Gecko/20100101 Firefox/45.0");
-	$mech->timeout(10);
-	$mech->default_header('Accept-Encoding' => scalar HTTP::Message::decodable());
 
 	$q =~ s/ /\+/g;
-	$mech->get("https://duckduckgo.com/?t=ffsb&q=$q&ia=web");
+	say "images: $q";
+	for (my $n=0; $n<length($q); $n++) {
+		print sprintf("%02x ",ord(substr($q,$n,1)));
+	}
+	say "";
+	my $c = http::myget("https://duckduckgo.com/?t=ffsb&q=$q&ia=web");
 
 	# avec duckduckgo, l'idée est de récupérer un champ généré pour
 	# produire une requête json, ça a beaucoup d'avantages, ça devrait
@@ -27,15 +27,11 @@ sub search {
 	# champ a l'air facile à récupérer, en haut d'une page de requête
 	# simple...
 
-	my $c = $mech->content;
 	my ($vqd) = $c =~ /vqd='(.+?)'/;
 
-	$mech->get("https://duckduckgo.com/i.js?l=fr-fr&o=json&q=$q&vqd=$vqd&f=,,,,,&p=1");
-	$self->{mech} = $mech;
+	$c = http::myget("https://duckduckgo.com/i.js?l=fr-fr&o=json&q=$q&vqd=$vqd&f=,,,,,&p=1");
 
 	# Décodage du js... !
-	my $c = $mech->content;
-	# $mech->save_content("page_images.html");
 	$c =~ s/^.+results"\:/{"results":/;
 	$c =~ s/,"vqd.+/}/;
 	my $json = decode_json($c);
@@ -43,7 +39,9 @@ sub search {
 	foreach (@$rtab) {
 		my %args;
 		($args{imgurl},$args{w},$args{h},$args{tbnid}) = ($_->{image},$_->{width},$_->{height},$_->{title});
-		$args{tbnid} =~ s/ /_/g; # utilisé en argument de la commande image, espaces interdits !
+		# tbnid est utilisé comme identifiant unique pour stocker dans le
+		# cache, espaces interdits, et | aussi...
+		$args{tbnid} =~ s/[ |\/]/_/g;
 
 		push @tab,\%args; # on garde tout, pourquoi se priver ?!!!
 
