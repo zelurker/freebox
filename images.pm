39a196fc573136fcbf843ae9c650156aea6db19a
Emmanuel Anne <emmanuel.anne@gmail.com>
2022-12-01 18:29:21 +0100

    ma solution pour l'instant Ã  duckduckgo images
    ils demandent une requÃªte gÃ©nÃ©rÃ©e par une tonne de javascript imbitable
    qui sort probablement de node.js. On a une solution radicale : demander
    Ã  firefox de le faire. Ca fera + de requÃªtes pour eux parce que firefox
    gÃ©nÃšre une tonne de requÃªtes en + qu'un simple script ici, mais c'est de
    leur faute, fallait pas commencer. Ca demande l'installation de firefox
    mais c'est quand mÃªme + light que le mod obÃšse sur cpan pour interagir
    directement avec un navigateur. Ca reste simple dans le principe :
    requÃªte pendant 10s directement sur la page d'images de duckduckgo avec
    la requÃªte qui va bien, Ã§a donne un panorama des images. AprÃšs le script
    commence normalement.

--- images.pm
+++ images.pm
@@ -13,7 +13,16 @@ sub search {
 	my ($self,$q) = @_;
 	my @tab = ();
 
-	$q =~ s/ /\+/g;
+	for (my $n=0; $n<length($q); $n++) {
+		if (ord(substr($q,$n,1)) < 64) {
+			$q = substr($q,0,$n)."%".sprintf("%02x",ord(substr($q,$n,1))).substr($q,$n+1);
+			$n += 2;
+		}
+	}
+	say "images: q=$q";
+
+	say "images: firefox...";
+	system("(firefox \"https://duckduckgo.com/?q=$q&t=h_&iax=images&ia=images\" &); sleep 10; killall firefox; cd ~/.mozilla/firefox; rm -f */places.sqlite-wal");
 	my $c = http::myget("https://duckduckgo.com/?q=$q&t=h_");
 
 	# avec duckduckgo, l'idée est de récupérer un champ généré pour
@@ -23,8 +32,31 @@ sub search {
 	# simple...
 
 	my ($vqd) = $c =~ /vqd='(.+?)'/;
+	my ($backend) = $c =~ /BackendDeepUrl\("(.+?)"/;
+	my ($nrj) = $c =~ /nrj\('(.+?)'/;
+	say "images: vqd = $vqd";
+	say "images: backend $backend";
+	say "images: nrj $nrj";
+#    $c = http::myget("https://links.duckduckgo.com$backend");
+	# A priori le déclencheur doit être dans l'une de ces requêtes, il faut peut-être les 2, seul pb : le seul paramètre que je peux retrouver c'est q, la requête, certains autres sont très mystérieux !
+	#http::myget("https://improving.duckduckgo.com/t/rq_0?8481373&r=1&tts=1&ac=0&rqv=1&q=$q&ttc=414299&ct=FR&d=d&kl=wt-wt&rl=us-en&kp=-1&serp_return=0&g=__&sm=wikipedia_fathead_deep:i:medium&blay=v1w2i1w26r1,e1w1&dsig=about:m&biaexp=b&deepsprts=b&eclsexp=b&msvrtexp=b");
+	#http::myget("https://improving.duckduckgo.com/t/webvitals?3392533&FCP=1696&TTFB=263&FID=1&has_performance=1&is_cached=0&navigation_type=navigate&has_back_data=1&is_loaded_from_bfcache=0&is_bounce_back=0&g=__&sm=wikipedia_fathead_deep:i:medium&blay=v1w2i1w26r1,e1w1&dsig=about:m&biaexp=b&deepsprts=b&eclsexp=b&msvrtexp=b");
+	#$c = http::myget("https://duckduckgo.com$nrj");
+	#	open(F,">nrj");
+	#	print F $c;
+	#	close(F);
+	#my ($load) = $c =~ /load_url":"(.+?)"/;
+	#say "images: load $load";
+	# $c = http::myget("$load");
+	# Il y a visiblement un truc tordu autour de cette load url, la requête vue du naviageur vers y.js (même qu'ici) n'a pas du tout ces paramètres
+	# donc je suppose qu'ils sont édités par la tonne de javascript qu'il y a autour... ! Ca renvoie des machins comme appid dans les paramètres et le truc
+	# est tellement tordu que ça n'a même pas besoin d'être réutilisé ensuite, ça reste valable quelques secondes, donc si on commence par faire ce genre de requête sur
+	# un navigateur et qu'on passe ici, ça marche, mais pas très longtemps... !
+	#    la requête nrj renvoie exactement la même réponse que backend, donc totalement inutile.
 
 	$c = http::myget("https://duckduckgo.com/i.js?l=fr-fr&o=json&q=$q&vqd=$vqd&f=,,,,,&p=1");
+	# la fameuse requête json qui renvoie tout, mais ça ne marche que si le machin a été validé par le fameux y.js ci-dessus (requête load), sinon ça renvoie un 403 (forbidden).
+	# Pour l'instant seule méthode trouvée : faire la requête par un navigateur d'abord... ! Pas terrible ouais...
 
 	# Décodage du js... !
 	$c =~ s/^.+results"\:/{"results":/;
