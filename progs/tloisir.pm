9d291a7333d911b4fc1d6f2181b7390b99b3fc7c
Emmanuel Anne <emmanuel.anne@gmail.com>
2025-08-11 20:12:33 +0200

    tloisir: fixes!
    les infos gÃ©nÃ©riques pouvaient inclure les rÃ©alisateurs mais avec
    balises html incluses, trÃšs illisibles.
    Et les Ã©toiles Ã©taient ajoutÃ©es Ã  chaque visualisation du programme!

--- progs/tloisir.pm
+++ progs/tloisir.pm
@@ -174,19 +174,22 @@ sub valid {
 		$sub = decode_entities($sub);
 		$$rtab[7] = $sub;
 	}
-	while ($r =~ s/li class="overview-stickerInfoListItem(.+?)<\/li>//s) {
+	while ($r =~ s/li class="overview-stickerInfoListItem">(.+?)<\/li>//s) {
 		my $info = $1;
 		$info =~ /strong>(.+)<\/strong>(.+)/;
 		$sub = "" if (!defined($sub));
 		$sub .= "$1$2\n";
 		$$rtab[7] = $sub;
 	}
-	while ($r =~ s/<span class="ratingsReport-typeTitle">(.+?)<\/span>.+?([\d\.]+)<\/span>//s) {
-		my $stars = "$1 : $2";
-		next if ($stars =~ /^votre note/i);
-		$$rtab[10] = "" if (!$$rtab[10]);
-		$$rtab[10] .= "\n" if ($$rtab[10]);
-		$$rtab[10] .= $stars;
+	if (!$$rtab[10]) {
+		while ($r =~ s/<span class="ratingsReport-typeTitle">(.+?)<\/span>.+?([\d\.]+)<\/span>//s) {
+			my $stars = "$1 : $2";
+			say "got stars $stars";
+			next if ($stars =~ /^votre note/i);
+			$$rtab[10] = "" if (!$$rtab[10]);
+			$$rtab[10] .= "\n" if ($$rtab[10]);
+			$$rtab[10] .= $stars;
+		}
 	}
 }
 
