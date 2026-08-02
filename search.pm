1a5979694d01321164e5f933ac95bed4eef1d8ed
Emmanuel Anne <emmanuel.anne@gmail.com>
2026-08-02 19:06:15 +0200

    migrating search to brave search
    duckduckgo is now useless
    expecting to have to migrate pictures soon too

--- search.pm
+++ search.pm
@@ -1,11 +1,19 @@
 package search;
 
+use common::sense;
 use WWW::Mechanize;
+use HTTP::Cookies;
+use HTML::Entities;
 
 sub init {
-	my $mech = WWW::Mechanize->new();
-	$mech->agent_alias("Linux Mozilla");
-	# $mech->agent("Mozilla/5.0 (X11; Linux x86_64; rv:57.0) Gecko/20100101 Firefox/57.0");
+	my $cookie_jar = HTTP::Cookies->new(
+           file => "$ENV{'HOME'}/lwp_cookies.dat",
+           autosave => 1,
+         );
+	my $mech = WWW::Mechanize->new(cookie_jar => $cookie_jar);
+	# $mech->agent_alias("Linux Mozilla");
+
+	$mech->agent("Mozilla/5.0 (X11; Linux x86_64; rv:152.0) Gecko/20100101 Firefox/152.0");
 	$mech->timeout(12);
 	$mech->default_header('Accept-Encoding' => scalar HTTP::Message::decodable());
 	$mech;
@@ -15,14 +23,10 @@ sub search {
 	my $q = shift;
 
 	my $mech = init();
+	$q =~ s/ /\+/g;
+	say "search: q=$q";
 	eval {
-		$mech->get("https://html.duckduckgo.com/html");
-		$mech->submit_form(
-			form_number => 1,
-			fields      => {
-				"q" => $q,
-			}
-		);
+		$mech->get("https://search.brave.com/search?q=$q");
 	};
 	return $mech;
 }
